9ea00d40e57e541831af922d424667e07c421b82
Jeonghwan Park <pjhisgod@naver.com>
2024-12-10 14:01:04 +0900

    fix: shapeFactory 싱글톤으로 수정

--- app/src/main/java/miridih/model/objects/factory/EllipseFactory.java
+++ app/src/main/java/miridih/model/objects/factory/EllipseFactory.java
@@ -4,6 +4,13 @@ import miridih.model.objects.EllipseShape;
 import miridih.model.objects.Shape;
 
 public class EllipseFactory implements ShapeFactory {
+  private static final EllipseFactory instance = new EllipseFactory();
+
+  private EllipseFactory() {}
+
+  public EllipseFactory getInstance() {
+    return instance;
+  }
 
   @Override
   public Shape createShape() {
--- app/src/main/java/miridih/model/objects/factory/RectangleFactory.java
+++ app/src/main/java/miridih/model/objects/factory/RectangleFactory.java
@@ -4,7 +4,13 @@ import miridih.model.objects.RectangleShape;
 import miridih.model.objects.Shape;
 
 public class RectangleFactory implements ShapeFactory {
+  private static final RectangleFactory instance = new RectangleFactory();
 
+  private RectangleFactory() {}
+
+  public static RectangleFactory getInstance() {
+    return instance;
+  }
   @Override
   public Shape createShape() {
     return new RectangleShape();
