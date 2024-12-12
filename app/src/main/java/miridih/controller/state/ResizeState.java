f49e6a6e04c5a352b8c335aa72b69085c69091f6
wonhj12 <wonhj12@naver.com>
2024-12-12 15:26:54 +0900

    mod: 드래그로 도형 크기 변경에 composite command 적용

--- app/src/main/java/miridih/controller/state/ResizeState.java
+++ app/src/main/java/miridih/controller/state/ResizeState.java
@@ -1,35 +1,48 @@
 package miridih.controller.state;
 
 import miridih.command.CommandInvoker;
+import miridih.command.CompositeCommand;
 import miridih.command.ResizeSelectedShapeCommand;
 import miridih.common.manager.PointManager;
 import miridih.controller.CanvasController;
 import miridih.model.CanvasModel;
 
 public class ResizeState extends ToolState {
-    public ResizeState(CanvasController canvasController, CanvasModel canvasModel) {
-        super(canvasController, canvasModel);
-    }
+  private CompositeCommand compositeCommand = null;
 
-    @Override
-    public void mousePressed(double x, double y) {
+  public ResizeState(CanvasController canvasController, CanvasModel canvasModel) {
+    super(canvasController, canvasModel);
+    if (compositeCommand == null) {
+      compositeCommand = new CompositeCommand(canvasModel);
     }
+  }
 
-    @Override
-    public void mouseReleased(double x, double y) {
-      this.canvasController.setState(new SingleSelectedState(this.canvasController, this.canvasModel));
-      PointManager.getInstance().setLastPoint(x, y);
+  @Override
+  public void mousePressed(double x, double y) {
+  }
+
+  @Override
+  public void mouseReleased(double x, double y) {
+    this.canvasController.setState(new SingleSelectedState(this.canvasController, this.canvasModel));
+    PointManager.getInstance().setLastPoint(x, y);
+
+    if (compositeCommand != null) {
+      CommandInvoker.getInstance().executeCommand(compositeCommand);
+      compositeCommand = null;
     }
+  }
 
-    @Override
-    public void mouseDragged(double x, double y) {
+  @Override
+  public void mouseDragged(double x, double y) {
+    if (compositeCommand != null) {
       ResizeSelectedShapeCommand command = new ResizeSelectedShapeCommand(canvasModel, x, y);
-      CommandInvoker.getInstance().executeCommand(command);
-      PointManager.getInstance().setLastPoint(x, y);
+      command.execute();
+      compositeCommand.addCommand(command);
     }
+    PointManager.getInstance().setLastPoint(x, y);
+  }
 
-    @Override
-    public void mouseClicked(double x, double y) {
-    }
-  
+  @Override
+  public void mouseClicked(double x, double y) {
+  }
 }
